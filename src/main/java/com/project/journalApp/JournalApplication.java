46245b12b28de37735d3b0ff3a645c991cea0b6b
Vamshidhar Reddy <vamshidhar1703@gmail.com>
2025-03-01 18:49:44 +0530

    Updated .gitignore and modified project files

--- src/main/java/com/project/journalApp/JournalApplication.java
+++ src/main/java/com/project/journalApp/JournalApplication.java
@@ -3,14 +3,43 @@ package com.project.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+
 
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableScheduling
 public class JournalApplication {
 
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(JournalApplication.class, args);
+        ConfigurableEnvironment environment = context.getEnvironment();
+        System.out.println(environment.getActiveProfiles()[0]);
 
-        SpringApplication.run(JournalApplication.class, args);
     }
+
+    @Bean
+    public PlatformTransactionManager manager(MongoDatabaseFactory dbfactory){
+        return new MongoTransactionManager(dbfactory);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return  new RestTemplate();
+    }
+
 
 
 }
